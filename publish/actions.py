724174d69156714b45d6e55f4b05dfca5bbd976b
Watt Iamsuri <watt@cookly.me>
2017-04-11 15:03:07 +0700

    util import

--- publish/actions.py
+++ publish/actions.py
@@ -1,7 +1,7 @@
 from django import template
 from django.core.exceptions import PermissionDenied
 from django.contrib.admin import helpers
-from django.contrib.admin.util import quote, model_ngettext, get_deleted_objects
+from django.contrib.admin.utils import quote, model_ngettext, get_deleted_objects
 from django.db import router
 from django.shortcuts import render_to_response
 from django.template.response import TemplateResponse
@@ -15,9 +15,10 @@ from django.contrib.admin.actions import delete_selected as django_delete_select
 from models import Publishable
 from utils import NestedSet
 
+
 def _get_change_view_url(app_label, object_name, pk, levels_to_root):
-    return '%s%s/%s/%s/' % ('../'*levels_to_root, app_label,
-                            object_name, quote(pk)) 
+    return '%s%s/%s/%s/' % ('../' * levels_to_root, app_label,
+                            object_name, quote(pk))
 
 
 def delete_selected(modeladmin, request, queryset):
@@ -28,6 +29,7 @@ def delete_selected(modeladmin, request, queryset):
     return django_delete_selected(modeladmin, request, queryset)
 delete_selected.short_description = "Mark %(verbose_name_plural)s for deletion"
 
+
 def undelete_selected(modeladmin, request, queryset):
     for obj in queryset:
         if not modeladmin.has_undelete_permission(request, obj):
@@ -37,12 +39,13 @@ def undelete_selected(modeladmin, request, queryset):
     return None
 undelete_selected.short_description = "Un-mark %(verbose_name_plural)s for deletion"
 
+
 def _get_publishable_html(admin_site, levels_to_root, value):
     model = value.__class__
     model_name = escape(capfirst(model._meta.verbose_name))
     model_title = escape(force_unicode(value))
     model_text = '%s: %s' % (model_name, model_title)
-    opts = model._meta                
+    opts = model._meta
 
     has_admin = model in admin_site._registry
     if has_admin:
@@ -51,12 +54,13 @@ def _get_publishable_html(admin_site, levels_to_root, value):
         url = _get_change_view_url(opts.app_label,
                                    opts.object_name.lower(),
                                    value._get_pk_val(),
- 	                               levels_to_root)
+                                   levels_to_root)
         html_value = mark_safe(u'<a href="%s">%s</a>' % (url, model_text))
     else:
         html_value = mark_safe(model_text)
-    
+
     return html_value
+
 
 def _to_html(admin_site, items):
     levels_to_root = 2
@@ -73,12 +77,13 @@ def _to_html(admin_site, items):
 def _convert_all_published_to_html(admin_site, all_published):
     return _to_html(admin_site, all_published.nested_items())
 
+
 def _check_permissions(modeladmin, all_published, request, perms_needed):
     admin_site = modeladmin.admin_site
 
     for instance in all_published:
         model = instance.__class__
-        other_modeladmin = admin_site._registry.get(model,None)
+        other_modeladmin = admin_site._registry.get(model, None)
         if other_modeladmin:
             if not other_modeladmin.has_publish_permission(request, instance):
                 perms_needed.append(instance)
@@ -93,14 +98,14 @@ def publish_selected(modeladmin, request, queryset):
     queryset = queryset.select_for_update()
     opts = modeladmin.model._meta
     app_label = opts.app_label
-    
+
     all_published = NestedSet()
     for obj in queryset:
         obj.publish(dry_run=True, all_published=all_published)
 
     perms_needed = []
     _check_permissions(modeladmin, all_published, request, perms_needed)
-    
+
     if request.POST.get('post'):
         if perms_needed:
             raise PermissionDenied
@@ -111,15 +116,15 @@ def publish_selected(modeladmin, request, queryset):
                 modeladmin.log_publication(request, object)
 
             queryset.publish()
-            
+
             modeladmin.message_user(request, _("Successfully published %(count)d %(items)s.") % {
                 "count": n, "items": model_ngettext(modeladmin.opts, n)
             })
             # Return None to display the change list page again.
             return None
-    
+
     admin_site = modeladmin.admin_site
- 
+
     context = {
         "title": _("Publish?"),
         "object_name": force_unicode(opts.verbose_name),
